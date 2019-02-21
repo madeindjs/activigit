689addc4028ebdadc5b28205d58b9e37dd41987c
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-02-21 17:22:14 +0100

    [+] Count number of commit per day

--- activigit.gemspec
+++ activigit.gemspec
@@ -34,7 +34,6 @@ Gem::Specification.new do |spec|
   spec.executables   = ['activigit.rb']
   spec.require_paths = ['lib']
 
-  spec.add_dependency 'git', '~> 1.5'
   spec.add_dependency 'gruff', '~> 0.7'
 
   spec.add_development_dependency 'bundler', '~> 1.17'
--- lib/activigit.rb
+++ lib/activigit.rb
@@ -1,17 +1,47 @@
 require 'activigit/version'
 require 'gruff'
-require 'git'
+require 'date'
 
 module Activigit
   class Error < StandardError; end
 
   def self.run
-    repository = Git.open('/home/apprenant/arousseau/website/')
+    logs = ''
 
-    repository.branches.each do |branch|
-      puts branch
-      puts repository.log.object(branch).count
+    Dir.chdir('/home/apprenant/arousseau/website/') do
+      logs = `git log --branches --format=%cI`.split("\n")
     end
+
+    logs_grouped = logs.group_by { |log| Date.parse(log).strftime('%Y-%m-%d') }
+
+    # logs_grouped.each { |x, y| print "#{x} #{y.size}\n" }
+
+    last_commit_date = Date.parse logs.first
+    current_date = Date.parse logs.last
+
+    counts = {}
+
+    while current_date != last_commit_date
+
+      current_date_str = current_date.strftime('%Y-%m-%d')
+
+      counts[current_date_str] = if logs_grouped[current_date_str].nil?
+                                   0
+                                 else
+                                   logs_grouped[current_date_str].count
+                                 end
+
+      current_date += 1
+
+    end
+
+    puts counts
+
+    # date_range = (first_commit_date..last_commit_date)
+    # days = date_range.uniq { |d| d }
+
+    # puts date_range.to_a.inspect
+    # puts last_commit_date
 
     return
 
